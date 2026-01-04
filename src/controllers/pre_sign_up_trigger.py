6faa7116258668feeab9f04a091db471374327b1
Victor França Lopes <18534956+vflopes@users.noreply.github.com>
2026-01-04 20:06:31 -0300

    test settings as global

--- src/controllers/pre_sign_up_trigger.py
+++ src/controllers/pre_sign_up_trigger.py
@@ -5,6 +5,8 @@ version: 1.0.0
 from src.settings import Settings
 import src.adapters.aws as aws_adapter
 
+from typing import Optional
+
 from aws_lambda_powertools.utilities.data_classes import event_source
 from aws_lambda_powertools.utilities.data_classes.cognito_user_pool_event import (
     PreSignUpTriggerEvent,
@@ -12,13 +14,18 @@ from aws_lambda_powertools.utilities.data_classes.cognito_user_pool_event import
 
 from src.facades import process_pre_sign_up
 
-settings = Settings.model_validate({})
-cognito_client = aws_adapter.get_cognito_client()
-dynamodb_client = aws_adapter.get_dynamodb_client()
+settings: Optional[Settings] = None
 
 
 @event_source(data_class=PreSignUpTriggerEvent)
 def lambda_handler(event: PreSignUpTriggerEvent, context):
+    global settings
+    if not settings:
+        settings = Settings.model_validate({})
+
+    cognito_client = aws_adapter.get_cognito_client()
+    dynamodb_client = aws_adapter.get_dynamodb_client()
+
     processed = process_pre_sign_up(
         event,
         settings,
